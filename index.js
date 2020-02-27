0e1ec3a0cceca94e8ac13bb1a1542843ac0c1b5e
Chloe Gell <gellert.chloe@gmail.com>
2020-02-27 18:18:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arr, event){
+  let newArr = []; 
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`); 
+  }
+  return newArr; 
+}
+
+function countDown(int){
+  let i = int; 
+  while (i >= 0) {
+    console.log(`${i}`); 
+    i--; 
+  }
+}
