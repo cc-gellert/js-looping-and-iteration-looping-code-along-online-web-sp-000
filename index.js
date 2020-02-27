2a3cb362ed0651aa30ab0eaae3f101d7c4209a29
Chloe Gell <gellert.chloe@gmail.com>
2020-02-27 18:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 function writeCards(arr, event){
+  newArr = []; 
   for(let i = 0; i < arr.length; i++){
     newArr.push(`Thank you, ${arr[i]}, for the wonderful birthday gift!`); 
   }
